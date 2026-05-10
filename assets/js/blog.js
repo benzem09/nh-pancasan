9d78e84b48453e94e2ccad700fdb22b45bca4e21
onkbaonk <onkbaonk@gmail.com>
2026-05-10 23:47:59 +0700

    first commit

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,5 @@
 let EDIT_POST_ID = null;
-const availableCategories = ["Semua", "Fiqih", "sharaf", "Nahu", "Hadist", "Umum"];
+const availableCategories = ["Semua",  "sharaf", "Nahu", "Tauhid", "Fiqih", "Tashauf", "Hadist", "Umum"];
 
 // 1. FUNGSI PEMBANTU (Helper)
 function getBlogIndexPath(year = new Date().getFullYear()) {
