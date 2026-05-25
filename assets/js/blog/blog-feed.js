70aac137b7a386e1b0a23d1041688d9566883141
onkbaonk <onkbaonk@gmail.com>
2026-05-26 03:07:45 +0700

    first commit

--- assets/js/blog/blog-feed.js
+++ assets/js/blog/blog-feed.js
@@ -41,7 +41,7 @@ async function refreshBlog(targetData = null, action = null) {
             const isOwner =
                 typeof CURRENT_USER !== "undefined" &&
                 (p.author === CURRENT_USER || CURRENT_USER === "admin");
-            const thumb = p.thumbnail || "assets/icons/articel.svg";
+            const thumb = p.thumbnail || "assets/icons/article.svg";
 
             return `
             <div class="glass post-card p-3 rounded-xl mb-3">
