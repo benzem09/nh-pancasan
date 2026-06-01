e1b17103825d10f3f8ca305eb4e223497c72ae93
onkbaonk <onkbaonk@gmail.com>
2026-06-01 16:52:28 +0700

    sitemap

--- assets/js/blog/sitemap.js
+++ assets/js/blog/sitemap.js
@@ -1,53 +1,57 @@
 async function generateSitemap() {
     try {
-        const posts = await loadAllIndexes();
+        const allPosts = await loadAllIndexes();
 
-        const now = new Date().toISOString();
+        const base = "https://benzem09.github.io/nh-pancasan";
 
-        let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        let xml =
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-<url>
-<loc>https://benzem09.github.io/nh-pancasan/</loc>
-<lastmod>${now}</lastmod>
-<priority>1.0</priority>
-</url>
+  <url>
+    <loc>${base}/</loc>
+    <priority>1.0</priority>
+  </url>
 `;
 
-        posts.forEach(post => {
-
-            const lastmod =
-                post.date
-                ? new Date(post.date).toISOString()
-                : now;
-
+        allPosts.forEach(post => {
             xml += `
-<url>
-<loc>https://benzem09.github.io/nh-pancasan/?post=${post.slug}</loc>
-<lastmod>${lastmod}</lastmod>
-<priority>0.8</priority>
-</url>
-`;
+  <url>
+    <loc>${base}/?post=${encodeURIComponent(post.slug)}</loc>
+    <priority>0.8</priority>
+  </url>`;
         });
 
         xml += `
 </urlset>`;
 
-        const file = await getGithubFile("sitemap.xml");
+        let sha = null;
+
+        try {
+            const file = await fetch(
+                `https://api.github.com/repos/${REPO_PATH}/contents/sitemap.xml`,
+                {
+                    headers: {
+                        Authorization: `token ${GITHUB_TOKEN}`
+                    }
+                }
+            );
+
+            if (file.ok) {
+                const data = await file.json();
+                sha = data.sha;
+            }
+        } catch {}
 
         await updateGithubFile(
             "sitemap.xml",
             xml,
-            file.sha,
+            sha,
             "Auto update sitemap"
         );
 
         console.log("Sitemap updated");
-    } catch(e) {
-        console.error("Generate sitemap gagal", e);
+    } catch (e) {
+        console.error("Sitemap gagal:", e);
     }
 }
