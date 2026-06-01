aeef01903f05ddfc79423ccd8a7a2115e8f2dcec
onkbaonk <onkbaonk@gmail.com>
2026-06-01 17:44:13 +0700

    sitemap js

--- assets/js/blog/sitemap.js
+++ assets/js/blog/sitemap.js
@@ -3,21 +3,34 @@ async function generateSitemap() {
         const allPosts = await loadAllIndexes();
 
         const base = "https://benzem09.github.io/nh-pancasan";
+        
+        // Ambil waktu saat ini untuk halaman utama / base URL
+        const currentDate = new Date().toISOString();
 
         let xml =
 `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 
   <url>
     <loc>${base}/</loc>
+    <lastmod>${currentDate}</lastmod>
     <priority>1.0</priority>
   </url>
 `;
 
         allPosts.forEach(post => {
+            // Jika objek post punya properti tanggal (misal: post.date atau post.updated), gunakan itu.
+            // Jika tidak ada, kita gunakan tanggal hari ini sebagai alternatif.
+            const postDate = post.updated || post.date || currentDate;
+            const formattedDate = new Date(postDate).toISOString();
+
             xml += `
   <url>
     <loc>${base}/?post=${encodeURIComponent(post.slug)}</loc>
+    <lastmod>${formattedDate}</lastmod>
     <priority>0.8</priority>
   </url>`;
         });
