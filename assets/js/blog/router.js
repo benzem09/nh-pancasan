2e01d8b585a6b8f4305b8571b0c49b83b0fa9a62
onkbaonk <onkbaonk@gmail.com>
2026-06-08 21:26:39 +0700

    update_router

--- assets/js/blog/router.js
+++ assets/js/blog/router.js
@@ -1,29 +1,29 @@
 async function checkUrlPost() {
+
     const params = new URLSearchParams(window.location.search);
     const slug = params.get('post');
 
     if (!slug) return;
 
     try {
-        const years = await getPublicFile('indices/years.json');
-        const fetchPromises = years.map(y =>
-            getPublicFile(`indices/index_${y}.json`)
-        );
 
-        const results = await Promise.all(fetchPromises);
-
-        let allPosts = [];
-        results.forEach(c => allPosts = allPosts.concat(c));
+        const allPosts = await loadAllIndexes();
 
         const found = allPosts.find(
             p => (p.slug || generateSlug(p.title)) === slug
         );
 
+        console.log("Slug URL :", slug);
+        console.log("FOUND :", found);
+
         if (found) {
-            loadFullPost(found.id);
+            await loadFullPost(found.id);
+        } else {
+            console.warn("Post tidak ditemukan:", slug);
         }
+
     } catch (err) {
-        console.error(err);
+        console.error("Router Error:", err);
     }
 }
 
